9cafb97a97fecc64820168b94330137c46244ace
FJVicVel03 <3021894750101@ingenieria.usac.edu.gt>
2024-03-15 16:05:09 -0600

    Commit Dos
    Como a la mitad xd

--- main.go
+++ main.go
@@ -9,26 +9,29 @@ import (
 	"strings"
 )
 
+// exec -path=/home/daniel/Escritorio/MIA_1S2024/Ejemplos_Proyecto/calificacion.script
+// exec -path=C:\Users\danie\Desktop\PRIMER_SEMESTRE_2024\MIA_1S2024\Ejemplos_Proyecto\calificacionWindows.script
+
+var logued = false
+
 func main() {
-	for true {
-		fmt.Println("\>>>>>>>>>>>>>>>>INGRESA UN COMANDO <<<<<<<<<<<<<<<<")
+	for {
+		fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>> INGRESE UN COMANDO <<<<<<<<<<<<<<<<<<<<<<<<<")
 		fmt.Println("--->>> Si desesa terminar con la aplicación ingrese \"exit\"")
-		fmt.Println("\t")
+		fmt.Print("\t")
 
 		reader := bufio.NewReader(os.Stdin)
 		entrada, _ := reader.ReadString('\n')
 		eleccion := strings.TrimRight(entrada, "\r\n")
-
 		if eleccion == "exit" {
 			break
 		}
 		comando := Comando(eleccion)
-
 		eleccion = strings.TrimSpace(eleccion)
 		eleccion = strings.TrimLeft(eleccion, comando)
 		tokens := SepararTokens(eleccion)
 		funciones(comando, tokens)
-		fmt.Println("\tPresione enter para continuar.")
+		fmt.Println("\tPresione Enter para continuar....")
 		fmt.Scanln()
 	}
 }
@@ -53,6 +56,7 @@ func Comando(text string) string {
 	}
 	return tkn
 }
+
 func SepararTokens(texto string) []string {
 	var tokens []string
 	if texto == "" {
@@ -122,6 +126,66 @@ func funciones(token string, tks []string) {
 		} else if Comandos.Comparar(token, "MKDISK") {
 			fmt.Println("*************************************** FUNCIÓN MKDISK **************************************")
 			Comandos.ValidarDatosMKDISK(tks)
+		} else if Comandos.Comparar(token, "RMDISK") {
+			fmt.Println("*************************************** FUNCIÓN RMDISK **************************************")
+			Comandos.RMDISK(tks)
+		} else if Comandos.Comparar(token, "FDISK") {
+			fmt.Println("*************************************** FUNCIÓN FDISK  **************************************")
+			Comandos.ValidarDatosFDISK(tks)
+		} else if Comandos.Comparar(token, "MOUNT") {
+			fmt.Println("*************************************** FUNCIÓN MOUNT  **************************************")
+			Comandos.ValidarDatosMOUNT(tks)
+		} else if Comandos.Comparar(token, "MKFS") {
+			fmt.Println("*************************************** FUNCIÓN MKFS  **************************************")
+			Comandos.ValidarDatosMKFS(tks)
+		} else if Comandos.Comparar(token, "LOGIN") {
+			fmt.Println("*************************************** FUNCIÓN LOGIN  **************************************")
+			if logued {
+				Comandos.Error("LOGIN", "Ya hay un usuario en línea.")
+				return
+			} else {
+				logued = Comandos.ValidarDatosLOGIN(tks)
+			}
+		} else if Comandos.Comparar(token, "LOGOUT") {
+			fmt.Println("*************************************** FUNCIÓN LOGOUT  **************************************")
+			if !logued {
+				Comandos.Error("LOGOUT", "Aún no se ha iniciado sesión.")
+				return
+			} else {
+				logued = Comandos.CerrarSesion()
+			}
+		} else if Comandos.Comparar(token, "MKGRP") {
+			fmt.Println("*************************************** FUNCIÓN MKGRP  **************************************")
+			if !logued {
+				Comandos.Error("MKGRP", "Aún no se ha iniciado sesión.")
+				return
+			} else {
+				Comandos.ValidarDatosGrupos(tks, "MK")
+			}
+		} else if Comandos.Comparar(token, "RMGRP") {
+			fmt.Println("*************************************** FUNCIÓN RMGRP  **************************************")
+			if !logued {
+				Comandos.Error("RMGRP", "Aún no se ha iniciado sesión.")
+				return
+			} else {
+				Comandos.ValidarDatosGrupos(tks, "RM")
+			}
+		} else if Comandos.Comparar(token, "MKUSER") {
+			fmt.Println("*************************************** FUNCIÓN MKUSER  **************************************")
+			if !logued {
+				Comandos.Error("MKUSER", "Aún no se ha iniciado sesión.")
+				return
+			} else {
+				Comandos.ValidarDatosUsers(tks, "MK")
+			}
+		} else if Comandos.Comparar(token, "RMUSR") {
+			fmt.Println("*************************************** FUNCIÓN RMUSER  **************************************")
+			if !logued {
+				Comandos.Error("RMUSER", "Aún no se ha iniciado sesión.")
+				return
+			} else {
+				Comandos.ValidarDatosUsers(tks, "RM")
+			}
 		} else {
 			Comandos.Error("ANALIZADOR", "No se reconoce el comando \""+token+"\"")
 		}
@@ -142,6 +206,7 @@ func FuncionExec(tokens []string) {
 	}
 	Exec(path)
 }
+
 func Exec(path string) {
 	file, err := os.Open(path)
 	if err != nil {
